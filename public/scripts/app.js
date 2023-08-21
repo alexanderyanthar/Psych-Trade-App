5efbb938b8cf37bb1773c49018e2a13b1da99646
alexanderyanthar <alexander.yanthar@outlook.com>
2023-08-20 21:32:11 -0400

    changed mongo cloud

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -64,6 +64,10 @@ document.getElementById('signup-Button').addEventListener('click', () => {
   window.location.href = 'http://psych-trade-app.eba-gawxqyim.us-east-1.elasticbeanstalk.com/signup';
 })
 
+// document.getElementById('signup-Button').addEventListener('click', () => {
+//   window.location.href = 'http://localhost:3000/signup';
+// })
+
 document.getElementById('login-btn').addEventListener('click', () => {
     window.location.href = 'http://psych-trade-app.eba-gawxqyim.us-east-1.elasticbeanstalk.com/login';
 })
