23c2534f897e319243f02ca6fd94a0b10e7ad304
alexanderyanthar <alexander.yanthar@outlook.com>
2023-08-21 08:58:33 -0400

    added assessment pages from previous psychtrade project

--- app.js
+++ app.js
@@ -208,6 +208,107 @@ app.get('/profile', (req, res) => {
     }
 });
 
+app.post('/logout', (req, res) => {
+    res.clearCookie('jwt');
+    res.redirect('/login');
+})
+
+app.get('/assessment', async (req, res) => {
+    try {
+        const token = req.cookies.jwt;
+
+        if (!token) {
+            return res.status(401).json({ message: 'Authentication required '});
+        }
+
+        jwt.verify(token, 'your-secret-key', async (err, decodedToken) => {
+            if (err) {
+                return res.status(403).json({ message: 'Invalid token'});
+            }
+
+            const userId = decodedToken.userId;
+            const questions = await Question.find().populate('options.points');
+            console.log(userId);
+
+            res.render('assessment', { questions } );
+        })
+    } catch (err) {
+        console.error('Error fetching assessment questions:', err);
+        res.status(500).send('Iternal Server Error');
+    }
+});
+
+app.post('/submitAssessment', async (req, res) => {
+    console.log('Form submitted')
+    try {
+        const token = req.cookies.jwt;
+
+        if (!token) {
+            return res.status(401).json({ message: 'Authenticaion Required' });
+        }
+
+        jwt.verify(token, 'your-secret-key', async (err, decodedToken) => {
+            if (err) {
+                return res.status(403).json({ message: 'Invalid token' });
+            }
+
+            const userId = decodedToken.userId;
+            const user = await User.findById(userId);
+
+            let fundamentalPoints = 0;
+            let technicalPoints = 0;
+            let hybridPoints = 0;
+            let userPreference = '';
+
+            for (const questionId in req.body) {
+                const selectedOptionPoints = parseInt(req.body[questionId]);
+                if (selectedOptionPoints === 1) {
+                    fundamentalPoints += 1;
+                } else if (selectedOptionPoints === 2) {
+                    technicalPoints += 1;
+                } else if (selectedOptionPoints === 3) {
+                    hybridPoints += 1;
+                }
+            }
+
+            if (fundamentalPoints > technicalPoints && fundamentalPoints > hybridPoints) {
+                userPreference = 'fundamental';
+            } else if (technicalPoints > fundamentalPoints && technicalPoints > hybridPoints) {
+                userPreference = 'technical';
+            } else if (hybridPoints > fundamentalPoints && hybridPoints > technicalPoints || fundamentalPoints === technicalPoints) {
+                userPreference = 'hybrid';
+            } else if (fundamentalPoints === hybridPoints) {
+                userPreference = 'hybrid-fundamnetal';
+            } else if (technicalPoints === hybridPoints) {
+                userPreference = 'hybrid-technical';
+            }
+
+            // console.log(`User preference: ${userPreference}`);
+            // console.log(`Points - Fundamental: ${fundamentalPoints}, Technical: ${technicalPoints}, Hybrid: ${hybridPoints}`);
+
+            const assessmentAnswers = new AssessmentAnswer({
+                user: userId,
+                userPreference: userPreference,
+                assessmentResults: [
+                    {type: 'fundamental', points: fundamentalPoints},
+                    {type: 'technical', points: technicalPoints},
+                    {type: 'hybrid', points: hybridPoints},
+                ]
+            });
+
+            await assessmentAnswers.save();
+
+            user.assessments.push(assessmentAnswers);
+            await user.save();
+
+            res.redirect('/profile');
+        })
+    } catch (err) {
+        console.error('Error submitting assessment:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
