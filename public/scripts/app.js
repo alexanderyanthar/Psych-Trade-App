eb6d4f073765b0b19d27a8a12b766bcfb9de4545
alexanderyanthar <alexander.yanthar@outlook.com>
2023-08-21 11:15:04 -0400

    added error handling on sign up form to display on front end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -60,22 +60,106 @@ hamburgerMenu.addEventListener('click', () => {
 // form submission
 // front-end JavaScript (front-end-js-file.js)
 
-document.getElementById('signup-Button').addEventListener('click', () => {
-  window.location.href = 'http://psych-trade-app.eba-gawxqyim.us-east-1.elasticbeanstalk.com/signup';
-})
-
 // document.getElementById('signup-Button').addEventListener('click', () => {
-//   window.location.href = 'http://localhost:3000/signup';
+//   window.location.href = 'http://psych-trade-app.eba-gawxqyim.us-east-1.elasticbeanstalk.com/signup';
 // })
 
-// document.getElementById('login-btn').addEventListener('click', () => {
-//   window.location.href = 'http://localhost:3000/login';
-// })
+document.getElementById('signup-Button').addEventListener('click', () => {
+  window.location.href = 'http://localhost:3000/signup';
+})
 
 
 document.getElementById('login-btn').addEventListener('click', () => {
     window.location.href = 'http://psych-trade-app.eba-gawxqyim.us-east-1.elasticbeanstalk.com/login';
 })
+
+// app.js or a separate JS file
+// document.getElementById('signup-form').addEventListener('submit', async (e) => {
+//     e.preventDefault(); // Prevent default form submission
+
+//     const form = e.target;
+//     const username = form.querySelector('#username').value;
+//     const password = form.querySelector('#password').value;
+
+//     try {
+//         const response = await fetch('/signup', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({ username, password })
+//         });
+
+//         const data = await response.json();
+
+//         const errorDiv = form.querySelector('#form-error');
+        
+//         if (response.ok) {
+//             // Redirect or handle successful signup
+//             window.location.href = '/profile';
+//         } else {
+//             errorDiv.textContent = data.errorMessage;
+//         }
+//     } catch (error) {
+//         console.error('Error submitting form:', error);
+//     }
+// });
+
+document.getElementById('signup-form').addEventListener('submit', async (e) => {
+    e.preventDefault(); // Prevent default form submission
+
+    const form = e.target;
+    const username = form.querySelector('#username').value;
+    const password = form.querySelector('#password').value;
+
+    const usernameError = form.querySelector('#username-error');
+    const passwordError = form.querySelector('#password-error');
+    const errorDiv = form.querySelector('#form-error');
+
+    usernameError.textContent = '';
+    passwordError.textContent = '';
+    errorDiv.textContent = '';
+
+    let hasError = false;
+
+    if (username.length < 6 || !/^[a-zA-Z0-9]+$/.test(username)) {
+        usernameError.textContent = 'Invalid username. Username must be at least 6 characters long with only letters and numbers.';
+        hasError = true;
+    }
+
+    if (password.length < 8 || !/(?=.*[0-9])(?=.*[@#$%^&+=]).*$/.test(password)) {
+        passwordError.textContent = 'Invalid Password. Password must be 8 Characters long and contain 1 symbol and number.';
+        hasError = true;
+    }
+
+    if (!hasError) {
+        try {
+            const response = await fetch('/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            });
+
+            const data = await response.json();
+
+            const errorDiv = form.querySelector('#error-message');
+
+            if (response.ok) {
+                // Redirect or handle successful signup
+                window.location.href = '/profile';
+            } else {
+                errorDiv.textContent = data.errorMessage;
+            }
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+    }
+});
+
+
+
 
 
 
