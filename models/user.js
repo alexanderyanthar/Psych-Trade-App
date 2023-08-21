12181c7b9463308590d67f9fdaedeeba572ddb2f
alexanderyanthar <alexander.yanthar@outlook.com>
2023-08-20 21:11:00 -0400

    migrated first part psych trade to new repo with new file structure

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    // assessments: [{ type: Schema.Types.ObjectId, ref: 'AssessmentAnswer' }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
